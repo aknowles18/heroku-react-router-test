a4a31f2d65e4e542dc855c02082755544c52a1f6
Alyson Knowles <aknowles18@kcimia.com>
2021-04-13 19:33:28 -0400

    Setting up react router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const Home = () => (
+  <div>
+    <h1>This is the home page</h1>
+  </div>
+);
+
+const AlternatePage = () => (
+  <div>
+    <h1>This is an Alternate page</h1>
+  </div>
+);
+
+const App = () => (
+  <BrowserRouter>
+    <div>
+      <Switch>
+        <Route path="/" component={Home} exact={true} />
+        <Route path="/alternate" component={AlternatePage} />
+      </Switch>
     </div>
-  );
-}
+  </BrowserRouter>
+);
 
 export default App;
